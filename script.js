b0c5de5defaf4774f3c2d44318ab4c178904ae3b
alexcengler <alexcengler@gmail.com>
2017-11-06 09:17:08 -0600

    Create javascript file, create spacing variables, svg, and g within chart div. Source JS file from index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+// Set Spacing Guidelines
+var margin = {top: 10, right: 10, bottom: 40, left: 40}
+var width = 850 - margin.left - margin.right;
+var height = 450 - margin.top - margin.bottom;
+
+// Create and Size SVG, where all of our visualization will appear:
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+// Create and place start of SVG g tag, inside which our heatmap will appear:
+var g = svg.append("g")
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
